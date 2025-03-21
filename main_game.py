7b8a5594cd29630179f9265777a9bf712778d78d
Hernan Provoste <hernanprovostev@outlook.com>
2025-03-21 14:01:33 -0300

    Add stage and letter array

--- main_game.py
+++ main_game.py
@@ -11,16 +11,93 @@ for position in range(len(chosen_word)):
     placeholder += "_"
 print(placeholder)
 
-guess = input("Guess a letter: ").lower()
-print(guess)
+# We create a loop to let the user guess again.
+game_over = False
+correct_letters = []
+lifes = 5
 
-# Create a display when player select a letter
-display = ""
+stages = [
+        """
+        --------
+        |      |
+        |      O
+        |     \|/
+        |      |
+        |     / \
+        -
+        """,
+        """
+        --------
+        |      |
+        |      O
+        |     \|/
+        |      |
+        |     /
+        -
+        """,
+        """
+        --------
+        |      |
+        |      O
+        |     \|/
+        |      |
+        |
+        -
+        """,
+        """
+        --------
+        |      |
+        |      O
+        |     \|
+        |      |
+        |
+        -
+        """,
+        """
+        --------
+        |      |
+        |      O
+        |      |
+        |      |
+        |
+        -
+        """,
+        """
+        --------
+        |      |
+        |      O
+        |
+        |
+        |
+        -
+        """,
+        """
+        --------
+        |      |
+        |
+        |
+        |
+        |
+        -
+        """
+]
 
-for letter in chosen_word:
-    if letter == guess:
-        display += letter
-    else:
-        display += "_"
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+    # Create a display when player select a letter
+    display = ""
 
-print(display)
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print(display)
+
+    if "_" not in display:
+        game_over = True
+        print("You win")
